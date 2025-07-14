4e81e851000e91f488b102f8bf567575c2095793
mohcinedev <mouhcinedev@gmail.com>
2025-07-14 18:10:04 +0100

    ui

--- views/LobbyView.js
+++ views/LobbyView.js
@@ -1,22 +1,47 @@
 import { createElement } from "../src/main.js";
 import { socket } from "../client.js";
 
+
+// https://excalidraw.com/#json=RJHu_-G6zzsMdhMu2waTM,Z0zJ3AAK6CPd9QM1WjLqew
+let defaultplayers = new Array(4).fill(null)
+
+
+function handleInput(e, sendToServer) {
+  if (e.key === "Enter") {
+    const input = e.target;
+    const text = input.value.trim();
+    if (text == '' || text.length > 100) {
+      alert('enter a msg between 1 and 100 char!!')
+    } else {
+      sendToServer({ type: "chat", text: text });
+      input.value = "";
+
+    }
+  }
+}
 export default function renderLobbyScreen(gameState, sendToServer) {
   const state = gameState.getState();
-  const lobbyPlayers = state.players ? Object.values(state.players) : [];
-  console.log(state);
+  const lobbyPlayers = Object.values(state.players || {})
 
-  const playersList =
-    lobbyPlayers.length > 0
-      ? lobbyPlayers.map((player) =>
-          createElement("li", {
-            children: [player.nickname || "Unnamed Player"],
-          })
-        )
-      : [createElement("li", { children: ["Waiting for players..."] })];
+  defaultplayers = defaultplayers.map((elem, i) => ({
+    nickname: lobbyPlayers[i]?.nickname || 'Waiting...'
+  }))
+
+  console.log(defaultplayers, defaultplayers.length);
+
+  const playersList = defaultplayers.map((player) =>
+    createElement("div", {
+      attrs: { class: 'player-card' },
+      children: [
+        createElement('span', {
+          children: [player.nickname]
+        })
+      ],
+    })
+  )
 
   const chatMessages = (state.chatMessages || []).map((msg) =>
-    createElement("p", {
+    createElement("span", {
       children: [
         createElement("strong", { children: [`${msg.nickname}: `] }),
         msg.text,
@@ -27,82 +52,80 @@ export default function renderLobbyScreen(gameState, sendToServer) {
   let lobbyContent;
 
   const sharedUI = [
-     createElement("button", {
-        attrs: { id: "quit-btn" },
-        children: ["Quit"],
-        events: {
-          click: () => {
-            console.log(123);
-
-            if (socket) {
-              socket.close();
-            }
-            sendToServer({ type: "quitGame" });
-            gameState.setState({
-              players: {},
-              bombs: [],
-              explosions: [],
-              gameOver: false,
-              winner: null,
-              gameStarted: false,
-              maze: null,
-              currentScreen: "join",
-              isPlayer1: false,
-              nickname: "",
-              chatMessages: [],
-            });
-            window.location.hash = "#/";
-          },
-        },
-      }),
-    createElement("h3", { children: ["Players"] }),
-    createElement("ul", { children: playersList }),
-    createElement("h3", {
-      children: ["Chat"],
-      attrs: { style: "margin-top: 20px;" },
-    }),
     createElement("div", {
-      attrs: { class: "chat-messages" },
-      children: chatMessages,
-    }),
-    createElement("input", {
-      attrs: {
-        type: "text",
-        id: "chat-input",
-        placeholder: "Type and press Enter...",
-        autocomplete: "off",
-      },
-      events: {
-        keypress: (e) => {
-          if (e.key === "Enter") {
-            const input = e.target;
-            const text = input.value.trim();
-            if (text) {
-              sendToServer({ type: "chat", text: text });
-              input.value = "";
-            }
-          }
-        },
-      },
+      attrs: { class: "chat-container" },
+
+      children: [
+        createElement('div', {
+          attrs: { class: 'chat-wrapper' },
+          children: [
+            createElement('div', {
+              attrs: { class: 'chat-messages' },
+              children: chatMessages
+            }),
+
+            createElement("input", {
+              attrs: {
+                type: "text",
+                id: "chat-input",
+                placeholder: "Type and press Enter..."
+
+              },
+              events: {
+                keypress: (e) => handleInput(e, sendToServer),
+              },
+            }),
+          ]
+        })
+
+      ],
     }),
   ];
 
   // if (state.isPlayer1) {
-    lobbyContent = [
-      createElement("header", {
-        attrs: { class: 'container' },
-        children: [
-          createElement("h2", { children: ["Lobby: You are the Host"] }),
-          createElement("p", { children: ['Click "Start Game" when ready.'] }),
-          createElement("button", {
-            attrs: { class: "btn btn-success" },
-            children: ["Start Game"],
-            events: { click: () => sendToServer({ type: "startGame" }) },
-          })
-        ]
-      }),
-      ...sharedUI,
-    ];
+  lobbyContent = [
+    createElement("header", {
+      attrs: { class: 'container' },
+      children: [
+        createElement("h2", { children: ["welcome to waiting area"] }),
+        createElement("button", {
+          attrs: { class: "btn btn-success" },
+          children: ["Start Game"],
+          events: { click: () => sendToServer({ type: "startGame" }) },
+        }),
+        createElement("button", {
+          attrs: { id: "quit-btn" },
+          children: ["Quit"],
+          events: {
+            click: () => {
+              console.log(123);
+
+              if (socket) {
+                socket.close();
+              }
+              sendToServer({ type: "quitGame" });
+              gameState.setState({
+                players: {},
+                bombs: [],
+                explosions: [],
+                gameOver: false,
+                winner: null,
+                gameStarted: false,
+                maze: null,
+                currentScreen: "join",
+                isPlayer1: false,
+                nickname: "",
+                chatMessages: [],
+              });
+              location.hash = "#/";
+            },
+          },
+        })
+      ]
+    }),
+    ...sharedUI,
+    ...playersList
+  ];
   // } else {
   //   lobbyContent = [
   //     createElement('header', {
